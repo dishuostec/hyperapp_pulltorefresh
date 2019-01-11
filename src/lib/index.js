ad378bed68dae44c5bb4efaacd626e3a4fbf5425
dishuostec <dishuostec@gmail.com>
2019-01-11 12:27:09 +0800

    remove default onRefresh

--- src/lib/index.js
+++ src/lib/index.js
@@ -48,7 +48,7 @@ const PullToRefresh = ({
         instructionsReleaseToRefresh,
         instructionsRefreshing,
         refreshTimeout,
-        onRefresh: () => console.log('refresh'),
+        onRefresh,
         resistanceFunction,
         shouldPullToRefresh,
       });
